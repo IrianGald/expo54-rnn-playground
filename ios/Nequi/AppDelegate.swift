a2c5852e25e1e2bcf8ef1cf291521bed62671eaf
Irian Gald <irian.gald@gmail.com>
2026-02-23 17:13:56 -0600

    feat: iOS added with navigation configured

---
+++ ios/Nequi/AppDelegate.swift
@@ -0,0 +1,37 @@
+import UIKit
+import React
+import React_RCTAppDelegate
+import ReactAppDependencyProvider
+//import Firebase
+import ReactNativeNavigation
+
+@UIApplicationMain
+class AppDelegate: RNNAppDelegate {
+  
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    self.reactNativeDelegate = ReactNativeDelegate()
+//    FirebaseApp.configure()
+
+    // Let RNNAppDelegate do its bootstrap and bridge creation
+    let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return result
+  }
+
+}
+
+class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
+  override func sourceURL(for bridge: RCTBridge) -> URL? {
+    self.bundleURL()
+  }
+  
+  override func bundleURL() -> URL? {
+#if DEBUG
+    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+#else
+    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
+  }
+}
